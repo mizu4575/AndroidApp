bd7703358a620a98ee34eaf6a3e49d6e27cc9349
mizu4575 <115239291+mizu4575@users.noreply.github.com>
2023-09-05 13:51:27 +0900

    テキスト入力をできるように変更

--- app/src/main/java/jp/ac/meijou/android/s221205036/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s221205036/MainActivity.java
@@ -3,12 +3,41 @@ package jp.ac.meijou.android.s221205036;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import jp.ac.meijou.android.s221205036.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            binding.textView.setText(text);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.textView.setText(s.toString());
+            }
+        });
     }
 }
